89802068a9541692dcb9bbec29ba10f2b4cd6496
SeeMemes <provotorov-02@mail.ru>
2021-11-15 03:34:32 +0300

    v1.12

---
+++ lab/threads/nlwpstater.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys # to get cmd options
+import subprocess as sp # to run external processes
+from lss import lsthreads, is_running, list_threads
+import re
+
+def list_threads( pid ):
+    if ( is_running( pid ) ):
+        return list( map( lambda tid: int( tid ),
+                    sp.run( lsthreads( pid ), check=True, stdout=sp.PIPE ).stdout.decode( 'UTF-8' ).splitlines() ) )
+    else:
+        return []
+
+def get_status( pid, tid ):
+    cmd = f'cat /proc/{pid}/task/{tid}/status | grep "State:"'
+    lsstate = sp.Popen( cmd, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT )
+    return lsstate.communicate( )[ 0 ]
+
+
+regex = re.compile( "State:\t(\D) .*" )
+
+
+# define main files snapshotting function
+def report( pid, interval, count ):
+    # is the main process running
+    pisrun = is_running( pid )
+    print( pid, ":", ( "running" if pisrun else "closed" ), file=sys.stderr )
+    if pisrun:
+        tids = list_threads( pid )
+        for tid in tids: # count threads
+            raw_state = get_status( pid, tid ).decode( "UTF-8" )
+            print( tid, re.search( regex, raw_state ).groups( 0 )[ 0 ] )
+
+    else:
+        exit( 0 )
